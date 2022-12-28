cec070ec63c67fcd033ccb32499309900639ad01
malakkuhail <malak2003@gmail.com>
2022-12-28 08:43:16 +0200

    init

---
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread thread=new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(2000);
+                    Intent goHome =new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivities(new Intent[]{goHome});
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }}};
+        thread.start();
+
+    }
+}
